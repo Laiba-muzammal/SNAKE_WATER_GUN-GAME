cc35654c3e0c952124e18105d1f7152724dd48b5
Laiba Muzammal <155936083+Laiba-muzammal@users.noreply.github.com>
2024-11-14 12:12:57 +0500

    Update snake_water_gun game.py

--- snake_water_gun game.py
+++ snake_water_gun game.py
@@ -1,10 +1,3 @@
-#Snake Water Gun Game:
-#RULES:
-#Snake vs. Water: Snake drinks water, so Snake wins.
-#Water vs. Gun: Gun sinks in water, so Water wins.
-#Gun vs. Snake: Gun shoots the snake, so Gun wins.
-#If both players choose the same option, it's a tie.
-
 import random
 opt="y"
 counter=0
